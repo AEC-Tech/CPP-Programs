f102f3f2d4ddf86901dd282e0d38acbac0db31f3
AEC-Tech <35284118+AEC-Tech@users.noreply.github.com>
2019-11-11 02:02:32 +0530

    Create Greatest_2.cpp

---
+++ Greatest_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+    int a,b,g;
+    cout<<"Enter two numbers ";
+    cin>>a>>b;
+    g = (a > b)?a:b;
+    cout<<"Greatest number is "<<g;
+    return 0;
+}
