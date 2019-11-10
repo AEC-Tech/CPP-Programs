785cf5c5679f03b0894a6868861569cf557d0fbd
AEC-Tech <35284118+AEC-Tech@users.noreply.github.com>
2019-11-11 02:01:52 +0530

    Create pass_fail.cpp

---
+++ pass_fail.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int marks;
+    
+    cout<<"Enter marks ";
+    cin>>marks;
+    (marks >= 50)?cout<<"Pass" : cout<<"Fail";
+    
+    return 0;
+}
