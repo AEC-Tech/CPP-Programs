b3765bd2996fc186a77b58c7a8b2946b4c7db185
AEC-Tech <35284118+AEC-Tech@users.noreply.github.com>
2019-11-11 14:23:08 +0530

    Create area_circle.cpp

---
+++ area_circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   float radius, area;
+   cout<<"Enter radius ";
+   cin>>radius;
+
+   area = 3.14 * radius * radius;
+   cout<<"Area is "<<area;
+    return 0;
+}
