a0755b6d2a5457db437c134fa5b06ddb820b7099
AEC-Tech <35284118+AEC-Tech@users.noreply.github.com>
2019-11-12 13:22:41 +0530

    Create driving_test.cpp

---
+++ driving_test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"Enter the age  ";
+    cin>>age;
+
+    (age>=18)?cout<<"Give Driving Test":cout<<"Come Later";
+    return 0;
+}
